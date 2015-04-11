9a1baa8f437bd0e48926c4cc520ac14a82911840
James Nguyen <jamesn6@uci.edu>
2015-04-11 13:20:06 -0700

    Retry Start Menu

---
+++ Extra Firm/Assets/UIManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+	public Canvas QuitMenu;
+	public Button startText;
+	public Button quitText;
+
+	void Start(){
+		QuitMenu = QuitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		quitText = quitText.GetComponent<Button> ();
+		QuitMenu.enabled = false;
+
+	}
+
+	public void QuitPress(){
+		QuitMenu.enabled = true;
+		startText.enabled = false;
+		quitText.enabled = false;
+	}
+
+	public void NoPress(){
+		QuitMenu.enabled = false;
+		startText.enabled = true;
+		quitText.enabled = true;
+	}
+
+	public void StartGame(){
+		Application.LoadLevel ("test_scene");
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
