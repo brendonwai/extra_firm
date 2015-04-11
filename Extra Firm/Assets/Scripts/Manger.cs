c6b45be803544c1360870e9e445de2f3e91ce917
Alex561 <alexchapp@yahoo.com>
2015-04-11 12:21:25 -0700

    Time Tracker

--- Extra Firm/Assets/Scripts/Manger.cs
+++ Extra Firm/Assets/Scripts/Manger.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Manger : MonoBehaviour {
 	public bool player1;
 	public bool player2;
+	public float elappsedTime;
 	// Use this for initialization
 	void Start () {
 		player1 = true;
@@ -16,6 +17,7 @@ public class Manger : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.T)) {
 			player1 = !player1;
 			player2 = !player2;
-		}
+			}
+		elappsedTime = Time.time;
 	}
 }
