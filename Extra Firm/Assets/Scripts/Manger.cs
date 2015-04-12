377d645eaa9dd446543e8da45a26fda76dff6f12
James Nguyen <jamesn6@uci.edu>
2015-04-11 17:54:55 -0700

    Player Turn Text
    added in main scene

--- Extra Firm/Assets/Scripts/Manger.cs
+++ Extra Firm/Assets/Scripts/Manger.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Manger : MonoBehaviour {
@@ -7,14 +8,15 @@ public class Manger : MonoBehaviour {
 	public float elappsedTime;
 	public bool player1Wins;
 	public bool player2Wins;
+	public Text turnText;
 
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
 	}
 
-
 	// Use this for initialization
 	void Start () {
+		turnText = GetComponent<Text>();
 		player1Turn = true;
 		player2Turn = false;
 	}
@@ -26,6 +28,11 @@ public class Manger : MonoBehaviour {
 			player1Turn = !player1Turn;
 			player2Turn = !player2Turn;
 			}
+
+		if (player1Turn)
+			turnText.text = "Player 1's Turn";
+		else if (player2Turn)
+			turnText.text = "Player 2's Turn";
 
 		elappsedTime = Time.time;
 	}
