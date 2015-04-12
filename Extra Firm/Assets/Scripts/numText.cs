796c44d3db575e5fd0d85a8e0f931bd9f88c3da7
Brendon Wai <brendonwai@dhcp-v102-147.mobile.uci.edu>
2015-04-11 18:38:07 -0700

    GUI Text for Regions

---
+++ Extra Firm/Assets/Scripts/numText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class numText : MonoBehaviour {
+
+	public GameObject Region;
+	Text displayText;
+	// Use this for initialization
+	void Start () {
+		displayText=GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		displayText.text=Region.GetComponent<RegionScript>().population.ToString();
+	}
+}
