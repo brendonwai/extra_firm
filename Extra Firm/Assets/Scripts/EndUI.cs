e409b77a749959d0675d10f96cdbf7cd1cba0cbb
James Nguyen <jamesn6@uci.edu>
2015-04-12 17:43:07 -0700

    Added Return Button at Endgame menu

---
+++ Extra Firm/Assets/Scripts/EndUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndUI : MonoBehaviour {
+	public Canvas endMenu;
+	public Button endText;
+	// Use this for initialization
+	void Start () {
+		endMenu = endMenu.GetComponent<Canvas> ();
+		endText = endText.GetComponent<Button> ();
+	}
+
+	public void ReturnPress(){
+		Application.LoadLevel ("menu_scene");
+	}
+}
